b4223e8e4b4a666882b6b27d193dcafb0a2484b5
law52525 <law52526@qq.com>
2018-05-26 00:59:17 +0800

    update many things

--- 新版/NewJwc.py
+++ 新版/NewJwc.py
@@ -1,11 +1,10 @@
 ﻿import requests
 import importlib
-import time
-from bs4 import BeautifulSoup
+from lxml import etree
 import sys
 import os
-from getpass import getpass
 from mail import SendEmail
+from getpass import getpass
 
 class Student:
     def __init__(self , number , password):
@@ -19,45 +18,30 @@ class CsuftJwc:
         #初始参数
         self.host = "http://jwgl.csuft.edu.cn"
         self.login = "http://authserver.csuft.edu.cn/authserver/login?service=http%3A%2F%2Fjwgl.csuft.edu.cn%2F"    # 教务处统一身份认证入口
-        self.checkScore = "http://jwgl.csuft.edu.cn/jsxsd/kscj/cjcx_frm"
+        self.checkNow = self.host+'/jsxsd/kscj/cjcx_frm'
         self.checkLevel = self.host+'/jsxsd/kscj/djkscj_list'
+        self.checkAll = self.host+'/jsxsd/kscj/cjcx_list'
         self.student = student
         self.session = requests.session()
         self.session.headers = {
             "Referer": "http://authserver.csuft.edu.cn/authserver/login?service=http%3A%2F%2Fjwgl.csuft.edu.cn%2F",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
         }
-        self.message = ""      # 成绩单信息
-        self.root = './html/'
+        self.root = './data/'
         if not os.path.exists(self.root):
             os.mkdir(self.root)
 
-    def show(self,trs):
-        for tr in trs:
-            for td in tr:
-                s = td.string
-                if s and s != "\n":
-                    print(s,end=' ')
-                    self.message += s+' '
-            print()
-            self.message += '\n'
-
     def getName(self, response):
-        html = response.content.decode("utf-8")
-        soup = BeautifulSoup(html, "html.parser")
-        body = soup.body
-        Nsb_pw = body.find('div', attrs={'class': 'Nsb_pw'})
-        Nsb_top = Nsb_pw.find('div', attrs={'class': 'Nsb_top'})
-        Nsb_top_menu = Nsb_top.find('div', attrs={'class': 'Nsb_top_menu'})
-        text = (str(Nsb_top_menu.find('div', attrs={'class': 'Nsb_top_menu_nc'})).split("\">")[1].split("(")[0])
+        html = etree.HTML(response.content.decode("utf-8"))
+        text = html.xpath("//div[@id='Top1_divLoginName']/text()")[0].split("(")[0]
         self.student.name = text
-        print("欢迎"+text+"同学!\n")
+        print("欢迎"+text+"同学!")
 
     def Login(self):
         # 访问教务系统
         response = self.session.get(self.login)
-        str = response.content.decode("utf-8")
-        lt = str[str.find("name=\"lt\" value=\""):str.find("-49sC-cas")].replace("name=\"lt\" value=\"","")+"-49sC-cas"
+        html = etree.HTML(response.content.decode("utf-8"))
+        lt = html.xpath("//form[@id='casLoginForm']/input[@name='lt']/@value")
         # 构造登录数据
         data = {
             'username': self.student.number,
@@ -74,40 +58,78 @@ class CsuftJwc:
             print("成功进入教务系统！")
         self.getName(response)
 
-    # 查本学期成绩
-    def getReport(self):
-        print("本学期成绩：",end='')
-        cjcx = self.session.post(self.checkScore).content.decode("utf-8")
-        str = cjcx[cjcx.find("cjcx_list_frm")+41:cjcx.find("cjcx_list_frm")+79]
-        html = self.session.post(self.host+str).content
-        with open(self.root+'本学期成绩.html', 'wb') as f:
-            f.write(html)
-        soup = BeautifulSoup(html,"lxml").find('div', attrs={'class': 'Nsb_pw Nsb_pw1'})
-        trs = soup.find_all("tr")
-        self.show(trs)
+    def ShowAndSave(self,ret,type):
+        print(type+"：")
+        with open(self.root+self.student.name+type+".txt","w",encoding="utf-8") as f:
+            for i in ret:
+                print(i)
+                f.write(i+"\n")
+        f.close()
 
-    #查等级考试成绩
+    # 查本学期成绩
+    def getNow(self):
+        html = self.session.post(self.checkNow).content
+        html = etree.HTML(html.decode("utf-8"))
+        time = html.xpath("//div[@class='Nsb_r_title']/text()")[0].split("询")[1]
+        data = self.getchoice(time)
+        self.ShowAndSave(data,"本学期成绩")
+
+    # 查等级考试成绩
     def getLevel(self):
-        print("等级考试成绩：")
+        ret = []
         html = self.session.post(self.checkLevel).content
-        with open(self.root+'等级考试成绩.html', 'wb') as f:
-            f.write(html)
-        soup = BeautifulSoup(html, "lxml").find('div', attrs={'class': 'Nsb_layout_r'})
-        trs = soup.find_all("tr")
-        self.show(trs)
+        html = etree.HTML(html.decode("utf-8"))
+        th = html.xpath("//table[@class='Nsb_r_list Nsb_table']/tr/th/text()")
+        th = th[0:2]+th[-3:]+[th[4]]
+        ret.append(' '.join(th))
+        tr = html.xpath("//table[@class='Nsb_r_list Nsb_table']/tr")
+        tr_len = len(tr)
+        for i in range(3,tr_len+1):
+            r = html.xpath("//table[@class='Nsb_r_list Nsb_table']/tr[" + i.__str__() + "]/td/text()")
+            ret.append(' '.join(r))
+        self.ShowAndSave(ret,"等级考试成绩")
+
+    # 查全部成绩
+    def getAll(self):
+        data = self.getchoice("")
+        self.ShowAndSave(data,"全部成绩")
+
+
+    # 根据开课时间查询成绩
+    def getchoice(self,kksj):
+        ret = []
+        data = {
+            'kksj':kksj,
+            'kcxz':'',
+            'kcmc':'',
+            'xsfs':'all'
+        }
+        html = self.session.post(self.checkAll,data=data).content
+        html = etree.HTML(html.decode("utf-8"))
+        th = html.xpath("//table[@class='Nsb_r_list Nsb_table Nsb_table_first']/tr[@class='tr2']/th/text()")
+        ret.append(' '.join(th))
+        tr = html.xpath("//table[@class='Nsb_r_list Nsb_table']/tr")
+        tr_len = len(tr)
+        for i in range(1,tr_len+1):
+            r = html.xpath("//table[@class='Nsb_r_list Nsb_table']/tr["+ i.__str__() +"]/td/text()")
+            score = html.xpath("//table[@class='Nsb_r_list Nsb_table']/tr["+ i.__str__() +"]/td/a/font/text()")
+            ret.append(' '.join(r[0:4]+score+r[4:]))
+        return ret
+
+    # 发邮件
+    def SendMail(self):
+        self.getNow()
+        with open(self.root + self.student.name + "本学期成绩" + ".txt", "r", encoding="utf-8") as f:
+            data = f.readlines()
+        SendEmail(''.join(data))
 
 if __name__ == "__main__":
-    user = input("学号：")     # 输入学号
-    pwd = getpass("密码：")    # 隐式输入密码
-    oldReportlen = 150         # 已经查看过的成绩的字符串长度
+    user = input("学号：")               # 输入学号
+    pwd = getpass("密码：")              # 隐式输入密码
     student = Student(user, pwd)
-    while True:               # 不断循环监控是否有新成绩
-        spider = CsuftJwc(student)  # 实例化爬虫
-        spider.Login()
-        spider.getReport()
-        if len(spider.message)>oldReportlen:    # 出了新成绩就立刻把新的成绩单写成邮件发给自己
-            SendEmail(spider.message)           # 发邮件
-            print("出新成绩了")
-            break
-        print("监控中...")
-        time.sleep(60)                          # 1分钟检测一次，防止太快了锁IP
+    spider = CsuftJwc(student)           # 实例化爬虫
+    spider.Login()                       # 登录教务系统
+    spider.getNow()                      # 获取本学期成绩
+    spider.getLevel()                    # 获取等级考试成绩
+    spider.getAll()                      # 获取全部成绩
+    # spider.SendMail()                    # 把成绩单通过邮件发出去
